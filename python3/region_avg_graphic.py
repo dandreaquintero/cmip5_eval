06277ecf6a18092a1d96c9243c508ee2b50ecdb9
Daniela Quintero <dandrea.quinterog@gmail.com>
2020-03-27 13:16:13 +0100

    Grafica annual average terminado (4 graficas: 2 de Alpin y 2 de Andes).

--- python3/region_avg_graphic.py
+++ python3/region_avg_graphic.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 import os.path
 
 
-def plot_time_series(data_in, param_in, region, title_in):
+def plot_time_series(data_in, param_in, region):
     '''
     plot_time_series ...
     '''
@@ -36,21 +36,22 @@ def plot_time_series(data_in, param_in, region, title_in):
     date = [cdftime.num2date(t) for t in time]
 
     # ############# A plot of field mean ##############
-    # plt.figure()
+
     plt.figure(region+' '+param_in, figsize=(15, 6))
     plt.plot(date, param_scaled[:, 0, 0], label=model)   # Plot original data
     # lowess = sm.nonparametric.lowess(param_scaled[:, 0, 0], time, frac=0.06)  # Filter lowess
     # plt.plot(date, lowess[:, 1], label=model)
 
-    window = 10  # date [x:-y], where x+y = window - 1
-    param_scaled_smoothed = moving_average(arr=param_scaled[:, 0, 0], win=window)
-    plt.plot(date[5:-4], param_scaled_smoothed, label=model+'_smoothed')  # Plot data smoothed
+    # window = 10  # date [x:-y], where x+y = window - 1
+    # param_scaled_smoothed = moving_average(arr=param_scaled[:, 0, 0], win=window)
+    # plt.plot(date[5:-4], param_scaled_smoothed, label=model+'_smoothed')  # Plot data smoothed
 
     plt.ylabel("%s (%s)" % (data_in.variables[param_in].long_name,
                             data_in.variables[param_in].units))
     plt.ticklabel_format(useOffset=False, axis='y')
     plt.xlabel("Time")
-    plt.title(data_in.variables[param_in].long_name + ' in the '+region+' region')
+    plt.title('Annual Average '+data_in.variables[param_in].long_name
+              + ' in the '+region+' region ')
 
 
 def avg_time_series(nc_in, param_in, region, box_in, model_in, print_info):
@@ -140,14 +141,15 @@ def avg_time_series(nc_in, param_in, region, box_in, model_in, print_info):
         print("number of data points: %d" % len(param_year))
         print("-"*80)
 
-    # title_fldmean = ('Field mean for ' + param_in + ' for ' + region + ' region'
-    #                  + ' for model ' + model_in)
+    #title_fldmean = ('Field mean for ' + param_in + ' for ' + region + ' region'
+#                  + ' for model ' + model_in)
 
-    title_ymean = ('Year and field mean for ' + param_in + ' for ' + region
-                   + ' region' + ' for model ' + model_in)
+    # title_ymean = ('Year and field mean for ' + param_in + ' for ' + region
+    #           + ' region' + ' for model ' + model_in)
 
-    # plot_time_series(data_fldmean, param_in, region, title_fldmean)
-    plot_time_series(data_ymean, param_in, region, title_ymean)
+    # plot_time_series (data_fldmeanmean, param_in, region, title_fldmean)
+    plot_time_series(data_ymean, param_in, region)
+    #plt.savefig(png_ymean, dpi=300)
 
     data_fldmean.close()
     data_ymean.close()
@@ -169,7 +171,7 @@ paramArray = []
 nc_files_dir = "../nc_files/"
 proyect_dir = "cmip5_converted/"
 
-max_models = 15
+max_models = 35
 # loop the regionArray and boxesArray together
 for region, box in zip(regionArray, boxesArray):
     i_models = 0
@@ -201,7 +203,7 @@ for region in regionArray:
         allaxes = fig.get_axes()
 
         colors = [colormap(i) for i in np.linspace(0, 1, len(allaxes[0].lines))]
-        linestyles = ['-', '-.']
+        linestyles = ['-']
         for i, j in enumerate(allaxes[0].lines):
             j.set_color(colors[i])
             j.set_linestyle(linestyles[i % len(linestyles)])
@@ -215,9 +217,7 @@ for region in regionArray:
         # plt.legend(loc='best')
         plt.legend(loc=(1.01, 0), fontsize='small', frameon=True)
         # plt.subplots_adjust(right=0.7)
-        plt.tight_layout(rect=[0,0,1,1])
-        # plt.draw()
+        plt.tight_layout(rect=[0, 0, 1, 1])
 
-        plt.savefig('../'+region+'_'+param+'.png', dpi=400)
-
+        plt.savefig('../'+region+'_'+param+'_annual_avg.png', dpi=400)
 plt.show()
